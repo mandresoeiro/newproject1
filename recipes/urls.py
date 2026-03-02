072f5f63f0e0ffbfcbed6cb77c29588d3a2bd452
Marcio Soeiro <mandre.soeiro@gmail.com>
2026-03-02 15:29:50 -0300

    feat: implementa a visualização detalhada da receita e estilização

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home), #namespace: é para diferenciar as urls de cada app, caso haja mais de um app com a mesma url, ex: home
+    path('recipes/', views.home), #namespace: é para diferenciar as urls de cada app, caso haja mais de um app com a mesma url, ex: home
     # path('contato/', views.contato),
     path('recipes/<int:id>/', views.recipe, name='recipe-view'), #namespace: é para diferenciar as urls de cada app, caso haja mais de um app com a mesma url, ex: recipe-view
 ]
